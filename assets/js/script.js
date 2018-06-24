da13d6ff2e84da17a17a71b42ea965e0a0d97b8c
lindsaym <lindsay.meaux@gmail.com>
2018-06-24 22:44:31 +0200

    couleur texte

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+function textGreen(){
+  document.getElementById('text').style.color = 'green';
+}
+function textBlue(){
+  document.getElementById('text').style.color = 'blue';
+}
+function textRed(){
+  document.getElementById('text').style.color = 'red';
+}
